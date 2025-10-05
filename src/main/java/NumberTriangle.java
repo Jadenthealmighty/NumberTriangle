e03cdf8e1018d640384b0a27e390a5697e548164
jadenal-aidroos <jjtortoise@icloud.com>
2025-10-04 23:33:49 -0400

    Fixed everything, trying to PR

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -54,7 +54,7 @@ public class NumberTriangle {
         return root;
     }
 
-//Wyyyyy
+//WNOOOO
     /**
      * [not for credit]
      * Set the root of this NumberTriangle to be the max path sum
